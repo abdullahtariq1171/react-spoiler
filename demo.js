cfca07572e6e9b8417a1b977878e946de0ae5806
Muhammad Abdullah <abdullahtariq52@gmail.com>
2018-10-18 14:27:45 +0500

    Update demo.js with more examples

--- demo.js
+++ demo.js
@@ -1,13 +1,35 @@
-import React, {Component} from 'react'
-import ReactSpoiler from "./lib/index";
+import React from 'react'
+import ReactSpoiler from "./src/index";
 
-export default class App extends Component {
+export default class App extends React.PureComponent {
   render() {
     return (
-      <div>
-        <ReactSpoiler blur={5}>
-          <h1> something good </h1>            
-        </ReactSpoiler>
+      <div style={{textAlign: 'center'}}>
+        <div>
+          <h2>With devault values</h2>
+          <ReactSpoiler>
+            <h1> Sample Text </h1>
+          </ReactSpoiler>
+        </div>
+
+        <hr />
+
+        <div>
+          <h2>hoverBlur={`{4}`}</h2>
+          <ReactSpoiler hoverBlur={4}>
+            <h1> Sample Text </h1>
+          </ReactSpoiler>
+        </div>
+
+        <hr />
+
+        <div>
+          <h2>with tag='h1'</h2>
+          <ReactSpoiler tag='h1'>
+            Sample Text
+          </ReactSpoiler>
+        </div>
+
       </div>
     )
   }
